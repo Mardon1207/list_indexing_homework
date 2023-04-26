bd896e5916a94eda50fd6638ccbacf2d0180aff6
Mardon1207 <mardonsultonov8@gmail.com>
2023-04-25 23:10:36 -0700

    8

--- List08.py
+++ List08.py
@@ -6,4 +6,14 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    i=0
+    while i<5:
+        if list1[i]==0:
+            list1[i]=False
+        else :
+            list1[i]=True
+        
+        i=i+1
+    return list1
+list1=[1,0,0,1,0]
+print(main(list1))
